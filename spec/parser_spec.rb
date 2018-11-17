0b8491d963c62bcf00f7dfb4dd42a11977c66cd1
Mark Delk <jethrodaniel@gmail.com>
2018-11-17 12:52:23 -0600

    move parser test data to fixture file

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,26 +2,14 @@
 
 require 'parser'
 
+require_relative '../spec/fixtures/sys_config.rb'
+
 describe Parser do
   let(:parser) { Parser.new 'spec/fixtures/sys_config.txt' }
 
-  let(:allocation) do
-    Matrix[ [0, 1, 0],
-            [2, 0, 0],
-            [3, 0, 2],
-            [2, 1, 1],
-            [0, 0, 2] ]
-  end
-
-  let(:max) do
-    Matrix[ [7, 5, 3],
-            [3, 2, 2],
-            [9, 0, 2],
-            [2, 2, 2],
-            [4, 3, 3] ]
-  end
-
-  let(:available) { Vector[3, 3, 2] }
+  let(:allocation) { SysConfig.allocation }
+  let(:max)        { SysConfig.max }
+  let(:available)  { SysConfig.available }
 
   let(:data) do
     { allocation: allocation, max: max, available: available }
