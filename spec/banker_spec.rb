6044bd3843e697dd894a357b89aa87914f4be0c0
Mark Delk <jethrodaniel@gmail.com>
2018-11-17 00:20:39 -0600

    add more examples

--- spec/banker_spec.rb
+++ spec/banker_spec.rb
@@ -2,11 +2,19 @@ require 'parser'
 require 'banker'
 
 describe Banker do
-  let(:data) { Parser.new('spec/fixtures/sys_config.txt').data }
+  let(:examples) do
+    [
+      Parser.new('spec/fixtures/sys_config.txt').data,
+      Parser.new('spec/fixtures/wikipedia_example.txt').data,
+      Parser.new('spec/fixtures/youtube_example.txt').data
+    ]
+  end
 
   describe '#safe?' do
     it 'checks whether a system is in a safe state' do
-      expect(Banker.safe?(**data)).to eq(true)
+      examples.each do |example|
+        expect(Banker.safe?(**example)).to eq(true)
+      end
     end
   end
 end
