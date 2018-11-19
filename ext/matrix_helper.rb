6c9eb912d4b6895e603b03bfb4e5ea5143e5c1da
Mark Delk <jethrodaniel@gmail.com>
2018-11-19 13:37:06 -0600

    make matrix helper raise exceptions, given bad args

--- ext/matrix_helper.rb
+++ ext/matrix_helper.rb
@@ -12,13 +12,17 @@ module MatrixHelper
 
   refine Matrix do
     def set_row(index, vector)
+      unless (0..row_size).cover?(index) && vector.size == column_size
+        raise ExceptionForMatrix::ErrDimensionMismatch
+      end
+
       rows[index] = vector
     end
   end
 
   refine Vector do
     def lt_or_eq_all?(vector)
-      throw ExceptionForMatrix::ErrDimensionMismatch unless size == vector.size
+      raise ExceptionForMatrix::ErrDimensionMismatch unless size == vector.size
       zip(vector).all? { |pair| pair.first <= pair.last }
     end
   end
--- spec/matrix_helper_spec.rb
+++ spec/matrix_helper_spec.rb
@@ -24,6 +24,21 @@ describe 'using MatrixHelper' do
         matrix.set_row 0, vector
         expect(matrix.row_vectors.first).to eq(vector)
       end
+
+      it 'raises an exception if the index is out of bounds' do
+        expect do
+          matrix.set_row -1, vector
+          matrix.set_row matrix.row_count + 1, vector
+        end.to raise_error(ExceptionForMatrix::ErrDimensionMismatch)
+      end
+
+      it 'raises an exception if the vector is not the right size' do
+        expect do
+          matrix.set_row 0, Vector[]
+          matrix.set_row 0, Vector[9]
+          matrix.set_row 0, Vector[9, 9, 9]
+        end.to raise_error(ExceptionForMatrix::ErrDimensionMismatch)
+      end
     end
   end
 
@@ -34,6 +49,13 @@ describe 'using MatrixHelper' do
 
       it 'whether each element is less than its corresponding element in arr' do
         expect(a.lt_or_eq_all?(b)).to eq(true)
+      end
+
+      it "raises an exception if the vectors aren't the same length" do
+        expect do
+          expect(a.lt_or_eq_all?(Vector[]))
+          expect(a.lt_or_eq_all?(Vector[1, 2, 3]))
+        end.to raise_error(ExceptionForMatrix::ErrDimensionMismatch)
       end
     end
   end
